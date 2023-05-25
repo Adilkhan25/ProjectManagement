a30cfe37e0908acfa2b773029a2072ca0d858cfc
Adil Khan <Adil03320@gmail.com>
2023-05-25 23:52:02 +0530

    set Up Sign in with the help of firebase

--- app/src/main/java/com/adilkhan/projemanage/activities/SignIn.kt
+++ app/src/main/java/com/adilkhan/projemanage/activities/SignIn.kt
@@ -1,12 +1,16 @@
 package com.adilkhan.projemanage.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.view.WindowManager
+import android.widget.Toast
 import com.adilkhan.projemanage.R
 import com.adilkhan.projemanage.databinding.ActivitySignInBinding
+import com.google.firebase.auth.FirebaseAuth
 
-class SignIn : AppCompatActivity() {
+class SignIn : BaseActivity() {
     private var bindingSignIn : ActivitySignInBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,9 @@ class SignIn : AppCompatActivity() {
             , WindowManager.LayoutParams.FLAG_FULLSCREEN
         )
         setupActionBar()
+        bindingSignIn?.btnSignIn?.setOnClickListener {
+            signInRegisteredUser()
+        }
     }
     // TODO (Step 7: A function for setting up the actionBar.)
     /**
@@ -34,4 +41,60 @@ class SignIn : AppCompatActivity() {
 
         bindingSignIn?.toolbarSignInActivity?.setNavigationOnClickListener { onBackPressed() }
     }
+    // TODO (Step 2: A function for Sign-In using the registered user using the email and password.)
+    // START
+    /**
+     * A function for Sign-In using the registered user using the email and password.
+     */
+    private fun signInRegisteredUser() {
+        // Here we get the text from editText and trim the space
+        val email: String = bindingSignIn?.etEmail?.text.toString().trim { it <= ' ' }
+        val password: String = bindingSignIn?.etPassword?.text.toString().trim { it <= ' ' }
+
+        if (validateForm(email, password)) {
+            // Show the progress dialog.
+            showProgressDialog(resources.getString(R.string.please_wait))
+
+            // Sign-In using FirebaseAuth
+            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener { task ->
+                    hideProgressDialog()
+                    if (task.isSuccessful) {
+
+                        Toast.makeText(
+                            this@SignIn,
+                            "You have successfully signed in.",
+                            Toast.LENGTH_LONG
+                        ).show()
+
+                        startActivity(Intent(this@SignIn, MainActivity::class.java))
+                    } else {
+                        Toast.makeText(
+                            this@SignIn,
+                            task.exception!!.message,
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+        }
+    }
+    // END
+
+    // TODO (Step 3: A function to validate the entries of a user.)
+    // START
+    /**
+     * A function to validate the entries of a user.
+     */
+    private fun validateForm(email: String, password: String): Boolean {
+        return if (TextUtils.isEmpty(email)) {
+            showErrorSnackBar("Please enter email.")
+            false
+        } else if (TextUtils.isEmpty(password)) {
+            showErrorSnackBar("Please enter password.")
+            false
+        } else {
+            true
+        }
+    }
+    // END
 }
